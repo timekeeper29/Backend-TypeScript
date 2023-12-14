2578b5d284928cb9232709a10005790c33298d18
timekeeper29 <nagatadaichi36@gmail.com>
2023-12-14 14:08:46 +0200

    basic implementation of signup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json()); 
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://RedditCloneDBUser:RedditCloneSecretPassword@cluster0.oinvs5o.mongodb.net/reddit-clone';
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000)) // listen only after connection is successful
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes); 
