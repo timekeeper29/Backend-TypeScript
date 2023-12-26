e9cb2553630d98ee333d318ae89df5edadf3a1bd
timekeeper29 <nagatadaichi36@gmail.com>
2023-12-26 15:04:29 +0200

    added posts structure

---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+	{
+
+	},
+	{ timestamps: true }
+);
+
+const Post = mongoose.model('post', postSchema);
+
+module.exports = Post;
