b9544afe4f5e34204f0b59fb074f9e0c2c862561
timekeeper29 <nagatadaichi36@gmail.com>
2023-12-26 17:53:29 +0200

    changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/index');
 const passport = require('passport');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.static('public')); // static files on the server
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use(passport.initialize());
 require('./config/localStrategy');
