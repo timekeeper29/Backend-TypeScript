9f2ef4f18ce234ac49d174c701b9968d8483884e
timekeeper29 <nagatadaichi36@gmail.com>
2023-12-28 11:27:45 +0200

    fixed test upload route

--- src/routes/testsRoutes.js
+++ src/routes/testsRoutes.js
@@ -1,15 +1,16 @@
 const { Router } = require('express');
-const router = Router();
 const requireJwtAuth = require('../middlewares/requireJwtAuth');
 const upload = require('../utils/multer/imageUpload');
 const multerErrorHandler = require('../utils/multer/errorHandler');
+
+const router = Router();
 
 router.get('/protected', requireJwtAuth, (req, res) => {
   res.send(`Hello, ${req.user.name}`);
 });
 
 
-router.post('/upload', upload.single(), multerErrorHandler, (req, res) => {
+router.post('/upload', upload.single('file'), multerErrorHandler, (req, res) => {
     if (!req.file) {
         return res.status(400).send({error: 'no file uploaded'});
     }
