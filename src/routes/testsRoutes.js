26301b12eca6d509f82b9d45c5da065f66b51f1d
timekeeper29 <nagatadaichi36@gmail.com>
2023-12-28 10:56:21 +0200

    fixed conflict problems

--- src/routes/testsRoutes.js
+++ src/routes/testsRoutes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const router = Router();
 const requireJwtAuth = require('../middlewares/requireJwtAuth');
-const upload = require('../middlewares/imageUpload');
 const upload = require('../utils/imageUpload');
 
 
