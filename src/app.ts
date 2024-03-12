ced08f83f8cbe89547f08699279764defd437511
timekeeper29 <nagatadaichi36@gmail.com>
2024-03-12 21:35:23 +0200

    added certificate reading for production

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ import routes from './routes/index';
 import cron from 'node-cron';
 import newsGenerator from './utils/newsGenerator';
 import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
 dotenv.config();
 
 // We need to export the createApp function for testing purposes
@@ -29,25 +31,37 @@ const createApp = (): Express => {
 
   // routes
   app.use('/', routes);
-  app.use('/public/images', express.static(join(__dirname, '../public/images')));
+  app.use(
+    '/public/images',
+    express.static(join(__dirname, '../public/images'))
+  );
 
   return app;
 };
 
-
 const startServer = () => {
   const app = createApp();
 
-	// swagger documentation setup
-	app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  // swagger documentation setup
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
   // database connection
   const dbURI = process.env.MONGODB_URI;
   mongoose
     .connect(dbURI)
     .then(() => app.listen(`${process.env.PORT}`))
-		.then(() => console.log(`Server running on port ${process.env.PORT}`))
+    .then(() => console.log(`Server running on port ${process.env.PORT}`))
     .catch((err) => console.log(err));
+
+  if (process.env.NODE_ENV === 'production') {
+    console.log('PRODUCTION');
+
+    const options = {
+      key: fs.readFileSync('../client-key.pem'),
+			cert: fs.readFileSync('../client-cert.pem'),
+    };
+		https.createServer(options, app).listen(443);
+  }
 };
 
 // Export the createApp function for testing purposes
@@ -57,7 +71,7 @@ export { createApp, startServer };
 if (require.main === module) {
   startServer();
 
-	// Schedule the task for every minute for testing
+  // Schedule the task for every minute for testing
   // cron.schedule('*/1 * * * *', () => {
   //   console.log('Running the news refresh task...');
   //   newsGenerator.run();
